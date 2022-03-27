7a3c18139a2f1f654ac4380cf459a92915732423
Guilherme Stein <guilherme.steink@gmail.com>
2022-03-27 13:02:25 -0300

    test

--- src/services/cron-job/routine.js
+++ src/services/cron-job/routine.js
@@ -9,7 +9,7 @@ class Routine
     {
         this.botgram = botgram;
         this.scrapper = scrapper;
-        this.cycle = "0 0 10 * * *"; // every day at 10:00 AM -> testing
+        this.cycle = "0 5 13 * * *"; // every day at 10:00 AM -> testing
         // this.cycle = '* * * *'; //every hour test;
         this.cronjob = cronjob
     }
