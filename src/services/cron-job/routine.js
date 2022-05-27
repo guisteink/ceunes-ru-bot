2fedc7f63dbaecfea8b7fcd8070129365238a08c
Guilherme Stein <guilherme.steink@gmail.com>
2022-05-27 11:08:30 -0300

    set test routine running

--- src/services/cron-job/routine.js
+++ src/services/cron-job/routine.js
@@ -11,12 +11,12 @@ class Routine
         this.scrapper = scrapper;
         // this.cycle = '45 13 * * *'; // -> 10:45 AM
 
-        this.testCycle = "* * * * *"; // all minute
+        // this.testCycle = "* * * * *"; // all minute
         // this.lunchCycle = '45 10 * * *'; // ?test -> 7:45 AM (fuso horario adaptado - AWS)
-        // this.lunchCycle = '00 11 * * *'; // ?test -> 8:00 AM (fuso horario adaptado - AWS)
-        this.dinnerCycle = '45 16 * * *'; // ?test -> 13:45 AM (fuso horario adaptado - AWS) 
+        this.lunchCycle = '00 11 * * *'; // ?test -> 8:00 AM (fuso horario adaptado - AWS)
+        this.dinnerCycle = '45 17 * * *'; // ?test -> 13:45 AM (fuso horario adaptado - AWS) 
 
-        this.lunchCycle = '06 14 * * *'; // ?test -> 8:00 AM (fuso horario adaptado - AWS)
+        // this.lunchCycle = '06 14 * * *'; // ?test -> 8:00 AM (fuso horario adaptado - AWS)
 
         this.cronjob = cronjob
     }
@@ -39,14 +39,14 @@ class Routine
             ])
         })
 
-        // cronjob.schedule(this.dinnerCycle, async () =>
-        // {
-        //     Promise.all([
-        //         await this.botgram.sendMessage("vix", "dinner", process.env.TELEGRAM_GROUP_TEST), //todo: set to vix group chat
-        //         await this.botgram.sendMessage("sm", "dinner", process.env.TELEGRAM_GROUP_TEST), //todo: set to sm group chat
-        //         await this.botgram.sendMessage("alegre", "dinner", process.env.TELEGRAM_GROUP_TEST) //todo: set to alegre group chat
-        //     ])
-        // })
+        cronjob.schedule(this.dinnerCycle, async () =>
+        {
+            Promise.all([
+                await this.botgram.sendMessage("vix", "dinner", process.env.TELEGRAM_GROUP_TEST), //todo: set to vix group chat
+                await this.botgram.sendMessage("sm", "dinner", process.env.TELEGRAM_GROUP_TEST), //todo: set to sm group chat
+                await this.botgram.sendMessage("alegre", "dinner", process.env.TELEGRAM_GROUP_TEST) //todo: set to alegre group chat
+            ])
+        })
 
         // await this.botgram.sendMessage("alegre", "lunch")
         // await this.botgram.sendMessage("dinner")
