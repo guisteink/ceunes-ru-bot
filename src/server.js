d02a23ada3133cecc010c206219969fee14c6355
Guilherme Stein <guilherme.steink@gmail.com>
2022-05-29 13:44:58 -0300

    add

--- src/server.js
+++ src/server.js
@@ -3,9 +3,13 @@ const port = process.env.PORT || 3000;
 const app = require("./app");
 const routine = require("./services/cron-job/routine");
 
+app.get("/healthy", (req, res) =>
+{
+    return res.json("Server is up!");
+});
+
 app.listen(port, () =>
 {
-    console.log("Server is now running! 🔥\n");
+    console.log(`Server is now running on ${port}!!! 🔥🔥🔥\n`);
     routine.execute()
-
 });
